586b0f60bf9d1ce01ab377a18f0aab987164a73b
Douglas Gomes <117647275+dege1douglas@users.noreply.github.com>
2023-07-03 11:48:38 -0300

    Add files via upload

---
+++ T r a Y r a T/app.js
@@ -0,0 +1,34 @@
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <title>Tradução de Site</title>
+// </head>
+// <body>
+//     <h1 id="titulo">Bem-vindo ao Meu Site</h1>
+    
+//     <button onclick="traduzir('en')">Inglês</button>
+//     <button onclick="traduzir('es')">Espanhol</button>
+//     <button onclick="traduzir('fr')">Francês</button>
+    
+//     <p id="conteudo">Este é o conteúdo do site.</p>
+    
+//     <script src="script.js"></script>
+// </body>
+// </html>
+
+{/* function traduzir(lang) {
+    var titulo = document.getElementById('titulo');
+    var conteudo = document.getElementById('conteudo');
+    
+    if (lang === 'en') {
+        titulo.innerHTML = 'Welcome to My Website';
+        conteudo.innerHTML = 'This is the website content.';
+    } else if (lang === 'es') {
+        titulo.innerHTML = 'Bienvenido a Mi Sitio Web';
+        conteudo.innerHTML = 'Este es el contenido del sitio web.';
+    } else if (lang === 'fr') {
+        titulo.innerHTML = 'Bienvenue sur Mon Site Web';
+        conteudo.innerHTML = 'Voici le contenu du site web.';
+    }
+} */}
